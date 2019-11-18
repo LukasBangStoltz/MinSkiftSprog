5f1f5037c58cdc2003a67c637cf0c91aaa595710
Lukas Bang Stoltz-Andersen <lukasbangstoltz@gmail.com>
2019-11-18 09:47:30 +0100

    First commit

---
+++ src/sprogskift/Engelsk.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sprogskift;
+
+/**
+ *
+ * @author thor
+ */
+public class Engelsk {
+
+	public void printMenu() {
+		System.out.println("Welcome");
+		System.out.println("1) Show dogs");
+		System.out.println("2) Choose dog");
+		System.out.println("3) Search dogs by name");
+		System.out.println("4) Change language");
+		System.out.println("9) Exit program");
+	}
+	public void printLang(){
+            System.out.println("Change language");
+            System.out.println("1) Danish");
+            System.out.println("2)English");
+        }
+}
--- src/sprogskift/SprogSkift.java
+++ src/sprogskift/SprogSkift.java
@@ -5,19 +5,43 @@
  */
 package sprogskift;
 
+import java.util.Scanner;
+
 /**
  *
  * @author thor
  */
 public class SprogSkift {
 
-	/**
-	 * @param args the command line arguments
-	 */
-	public static void main(String[] args) {
-		Dansk sprog = new Dansk();
-		sprog.printMenu();
-		// print en menu
-	}
-	
+    /**
+     * @param args the command line arguments
+     */
+   static Dansk sprog;
+   static Scanner sc;
+    public static void main(String[] args) {
+        Dansk sprog = new Dansk();
+        sprog.printMenu();
+        int choice = 0;
+        Scanner sc = new Scanner(System.in);
+        while (choice != 9) {
+            choice = sc.nextInt();
+            switch (choice) {
+                case 4:
+                    changeLanguage();
+                    break;
+                default:
+                    choice = 9;
+                    break;
+            }
+        }
+    }
+    public static void changeLanguage(){
+        sprog.printLang();
+        int langchoice = sc.nextInt();
+        switch(langchoice){
+            case 1: sprog = new Dansk();break;
+//            case 2: sprog = new Engelsk();break;
+        }
+        
+    }
 }
